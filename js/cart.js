ade261f89581e00d5c6787f1263567ee0ea984cb
crossiello <claudio.rossiello@hotmail.com>
2021-10-06 20:32:15 -0300

    prueba

--- js/cart.js
+++ js/cart.js
@@ -1,6 +1,30 @@
+var carritoArray = [];
+
+function showCarritoList(array) {
+
+    let htmlContentToAppend = "";
+    for (let i = 0; i < array.length; i++) {
+        let articulo = array[i];
+
+        htmlContentToAppend += articulo.name + `<br>` + articulo.count + `<br>` + articulo.unitCost + `<br>` + articulo.currency + `<br>` + `subtotal: ` + (articulo.unitCost * articulo.count) + `<img src="` + articulo.src + `" alt="` + articulo.name + `" width="256px" height="156px"> <hr><br><br>`
+
+    }
+    document.getElementById("carrito").innerHTML = htmlContentToAppend;
+}
+
+
+
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
-document.addEventListener("DOMContentLoaded", function(e){
+document.addEventListener("DOMContentLoaded", function(e) {
+    getJSONData(CART_INFO_URL2).then(function(resultado) {
+        if (resultado.status === "ok") {
+            carritoArray = resultado.data;
 
+            showCarritoList(carritoArray.articles);
+
+        }
+
+    });
 });
--- js/init.js
+++ js/init.js
@@ -8,6 +8,7 @@ const PRODUCT_INFO_URL2 = "https://crossiello.github.io/Claudio-Rossiello-reposi
 const PRODUCT_INFO_COMMENTS_URL = "https://japdevdep.github.io/ecommerce-api/product/5678-comments.json";
 const COMENTARIOS = "https://crossiello.github.io/Claudio-Rossiello-repositorio-JaP/comentarios.json";
 const CART_INFO_URL = "https://japdevdep.github.io/ecommerce-api/cart/987.json";
+const CART_INFO_URL2 = "https://japdevdep.github.io/ecommerce-api/cart/654.json";
 const CART_BUY_URL = "https://japdevdep.github.io/ecommerce-api/cart/buy.json";
 
 var showSpinner = function() {
