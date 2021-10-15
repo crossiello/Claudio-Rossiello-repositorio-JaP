e13c1becf0de446c8a6ca64934a6c3c32a357808
crossiello <claudio.rossiello@hotmail.com>
2021-10-14 23:14:22 -0300

    aaaa

--- js/cart.js
+++ js/cart.js
@@ -1,5 +1,42 @@
 var carritoArray = [];
 
+function calcTotal() {
+    let total = 0;
+    let lista = "<br>";
+    let subs = document.getElementsByClassName("subtotal");
+    let names = document.getElementsByClassName("nombreArticulo");
+    let cantidades = document.getElementsByClassName("cantidad");
+    let monedas = document.getElementsByClassName("moneda");
+    for (let i = 0; i < subs.length; i++) {
+        if (cantidades[i].value != 0) {
+            if (monedas[i].innerHTML == "USD") {
+                lista += `-` + names[i].innerHTML + ` x` + cantidades[i].value + `<br> USD` + subs[i].innerHTML + `<br><br>`;
+
+                total += parseInt(subs[i].innerHTML);
+            } else {
+                lista += `-` + names[i].innerHTML + ` x` + cantidades[i].value + `<br> UYU` + subs[i].innerHTML + `<br><br>`;
+
+                total += parseInt(subs[i].innerHTML) / 40;
+            }
+
+        }
+    }
+    lista += `<strong>-Costo envío:</strong> ` + getEnvio() + `% <hr class="raya">`;
+    total = total + ((total / 100) * getEnvio());
+    document.getElementById("listaCompras").innerHTML = lista;
+    document.getElementById("listaCompras").innerHTML += `<h3>USD ` + total + `</h3>`;
+
+}
+
+function calcSubTotal(precio, i) {
+    let cantidad = parseInt(document.getElementById(`cantidadArticulo` + i).value);
+    subtotal = precio * cantidad;
+    document.getElementById(`subtotalArticulo` + i).innerHTML = subtotal;
+    calcTotal();
+
+
+}
+
 function showCarritoList(array) {
 
     let htmlContentToAppend = "";
@@ -19,9 +56,9 @@ function showCarritoList(array) {
         <div class="col-2></div>
         <div class="col>
       <div class="card-body">
-        <h3 class="card-title"> <strong>` + articulo.name + `</strong></h3>
-        <p class="card-text"> Precio unitario ` + articulo.currency + articulo.unitCost + `<br>` + ` Cantidad <input type="number" id="cantidadArticulo` + i + `" value="` + articulo.count + `" min="0" max="100" step="1"/></p>
-        <p class="card-text"><small class="text-muted"> subtotal: ` + (articulo.unitCost * articulo.count) + `</small></p>
+        <h3 class="nombreArticulo"> <strong>` + articulo.name + `</strong></h3>
+        <p class="card-text"> Precio unitario ` + articulo.currency + articulo.unitCost + `<br>` + ` Cantidad <input type="number" onchange="calcSubTotal(` + articulo.unitCost + `,` + i + `)" id="cantidadArticulo` + i + `" class="cantidad" value="` + articulo.count + `" min="0" max="100" step="1"/></p>
+        <h5 > Subtotal: <span class="moneda">` + articulo.currency + `</span><span class="subtotal" id="subtotalArticulo` + i + `">` + (articulo.unitCost * articulo.count) + `</span> </h5>
       </div>
     </div>
   </div>
@@ -31,47 +68,18 @@ function showCarritoList(array) {
 
     }
     document.getElementById("carrito").innerHTML = htmlContentToAppend;
+    calcTotal();
+}
+
+function mostrarCompra() {
+    alert("Su compra ha sido realizada con éxito");
 }
 
 
-function showListaCarrito(array) {
-
-    let lista = "<br>";
-    for (let i = 0; i < array.length; i++) {
-        let articulo = array[i];
-
-        lista += `-` + articulo.name + ` ` + articulo.unitCost * document.getElementById("cantidadArticulo" + i).value + `<br><br>`
-
-    }
-
-    lista += `<hr class="raya">`
-
-    document.getElementById("listaCompras").innerHTML = lista;
-}
-
-function sumaCarrito(array) {
-
-
-    var total = 0;
-    for (let i = 0; i < array.length; i++) {
-        let articulo = array[i];
-        if (articulo.currency == "UYU") {
-
-            total += (articulo.unitCost * document.getElementById("cantidadArticulo" + i).value) / 40;
-
-        } else
-            total += (articulo.unitCost * document.getElementById("cantidadArticulo" + i).value)
 
 
 
 
-
-
-
-    }
-
-    document.getElementById("listaCompras").innerHTML += `Total: ` + total;
-}
 
 //Obtengo la puntuación que puso el usuario
 function getEnvio() {
@@ -96,11 +104,10 @@ function mostrarEnvios() {
 document.addEventListener("DOMContentLoaded", function(e) {
     getJSONData(CART_INFO_URL2).then(function(resultado) {
         if (resultado.status === "ok") {
-            carritoArray = resultado.data;
+            carritoArray = resultado.data.articles;
 
-            showCarritoList(carritoArray.articles);
-            showListaCarrito(carritoArray.articles);
-            sumaCarrito(carritoArray.articles);
+            showCarritoList(carritoArray);
+
 
         }
 
