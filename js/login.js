78e1663410f21296e905967b2d0039186aec15c6
crossiello <claudio.rossiello@hotmail.com>
2021-10-31 01:39:05 -0300

    aaa

--- js/login.js
+++ js/login.js
@@ -1,47 +1,35 @@
+function guardar() {
+    let datos_u = {
+        email: document.getElementById("email").value
+    };
+
+    let datos_u_json = JSON.stringify(datos_u);
+
+    localStorage.setItem("datos_user", datos_u_json);
+    window.location = "inicio.html";
+}
+
+//VALIDACIÓN
+
+let form = document.getElementById('needs-validation');
+
+form.addEventListener('submit', function(e) {
+    if (!form.checkValidity()) {
+        e.preventDefault();
+        e.stopPropagation();
+    } else {
+        e.preventDefault();
+        guardar();
+
+
+    }
+    form.classList.add('was-validated');
+})
+
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
 document.addEventListener("DOMContentLoaded", function(e) {
 
-    document.getElementById("boton").addEventListener("click", function() {
-
-        let inputEmail = document.getElementById("inputEmail");
-        let inputPassword = document.getElementById("inputPassword");
-        let camposCompletos = true;
-
-        if (inputEmail.value === '') {
-            inputEmail.classList.add("invalid");
-            camposCompletos = false;
-        } else {
-            inputEmail.classList.remove("invalid");
-        }
-
-        if (inputPassword.value === '') {
-            inputPassword.classList.add("invalid");
-            camposCompletos = false;
-        } else {
-            inputPassword.classList.remove("invalid")
-        }
-
-        if (camposCompletos) {
-            window.location = 'inicio.html';
-        } else {
-            alert("Debe ingresar los datos")
-        }
-    });
 
 });
-
-
-function guardarDatos() {
-
-    //Creo un objeto que contega los datos a guardar:
-    let datos_u = {
-        email: document.getElementById("inputEmail").value
-    };
-
-    let datos_u_json = JSON.stringify(datos_u);
-
-
-    localStorage.setItem("datos_user", datos_u_json);
-}
